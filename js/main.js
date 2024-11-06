f9dece2e5ec1357631d06c1027fc4a346b8ff77a
Ecumbe <eduardo.cumbev@ug.edu.ec>
2024-11-06 08:49:37 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 // js/main.js
 
 document.getElementById('login-btn').addEventListener('click', () => {
-    window.location.href = "luxury/login.html";  // Redirige a la pantalla de login
+    window.location.href = "login.html";  // Redirige a la pantalla de login
 });
